f7c3da3147aba19ff03c0aa13edc3ac64eb88945
Lewis <Chris.Lewis@nuveen.com>
2017-03-02 13:47:12 -0600

    Added Test-Group.ps1

--- Test-Group.ps1
+++ Test-Group.ps1
@@ -1,6 +1,6 @@
-﻿<#
+<#
 .SYNOPSIS
-Creates a Zerto VPG for the given migration group
+Tests the VPG and NSM lists for a  given migration group
 
 .DESCRIPTION
 The Make-GroupVPG.ps1 script creates a VPG for a given migration group.  The Script reads the VPG from the VPGList and then 
@@ -51,12 +51,9 @@ Displays the version number and exits
 .NOTES
 Author: Chris Lewis
 Date: 2017-03-02
-Version: 1.2
+Version: 1.1
 History:
     1.0 Initial create
-    1.1 Dynamic Datastores
-    1.2 Validation of rights to NSM/VPG files, Zerto rights
-
 
 #>
 
@@ -65,13 +62,8 @@ History:
 param(
     [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "VPG Group Name")] [String] $GroupName,
     [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "VPG Migration Type")]  [ValidateSet('Mig','DR','FP')] [string] $MigrationType,
-    [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "Zerto Source Server")] [String] $ZertoSourceServer,
-    [parameter(Mandatory = $False, ParameterSetName = 'Default', HelpMessage = "Zerto Source Server Port")] [int] $ZertoSourceServerPort = 9669,
-    [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "Zerto Source User")] [String] $ZertoUser,
-    [parameter(Mandatory = $False, ParameterSetName = 'Default', HelpMessage = "Commit VPG")] [Switch] $CommitVPG,
     [parameter(Mandatory = $true,  ParameterSetName = 'Version', HelpMessage = "Version")] [Switch] $Ver
 )
-#Add-Type -TypeDefinition "public enum VPGMigrationType { MigVPG, DRVPG, FPVPG }"
 
 function Get-Datastore {
     param (
@@ -116,38 +108,11 @@ function Get-Datastore {
     return $DatastoreName
 }
 
-$ScriptVersion = "1.2"
-$MinZertoModuleVersion = [Version]"0.9.13"
+
+$ScriptVersion = "1.1"
 
 if ( $ver) {
     Write-Host ($MyInvocation.MyCommand.Name + " - Version $ScriptVersion")
-    Return
-}
-
-#This downloads the ZertoModule from PowerShell Gallery - you may need to run this first
-#Install-Module -Name ZertoModule
-
-#AutoImport doesn't work for Script modules
-Import-Module ZertoModule
-#Import-Module C:\Scripts\Zerto\ZertoModule\ZertoModule.psd1
-If ((get-module -Name 'ZertoModule') -eq $null ) {
-    throw "Could not find ZertoModule - please install it first via 'Install-Module -Name ZertoModule'"
-    Return
-}
-If ( (Get-Module 'ZertoModule').Version -lt $MinZertoModuleVersion ) {
-    throw "You must use at least ZertoModule $MinZertoModuleVersion.  Please upgrade with 'Update-Module ZertoModule'"
-    Return
-}
-
-#Log Into Zerto
-Set-Item ENV:ZertoServer $ZertoSourceServer
-Set-Item ENV:ZertoPort $ZertoSourceServerPort
-Set-ZertoAuthToken -ZertoUser $ZertoUser
-
-Try {
-    Get-ZertoLocalSite
-} catch {
-    throw "Cound not log onto ZertoServer at '$ZertoSourceServer'"
     Return
 }
 
@@ -166,14 +131,16 @@ If ( -not (Test-Path $NSMSource) ) {
 $VPGData = Import-Csv -Path $VPGSource | Where-Object {$_.ZertoVPG -eq $GroupName}
 If ($VPGData -eq $null) { throw "Invalid VPG Group Name" }
 
+#This VPG's NSM
 Switch ($MigrationType) {
     'Mig' { $NSMData = Import-Csv -Path $NSMSource | Where-Object {$_.MigVPG -eq $GroupName} }
     'DR'  { $NSMData = Import-Csv -Path $NSMSource | Where-Object {$_.DRVPG -eq $GroupName} }
     'FP'  { $NSMData = Import-Csv -Path $NSMSource | Where-Object {$_.FPVPG -eq $GroupName} }
     Default {throw "Invalid Migration Type '$MigrationType'"}
 }
-
 If ( ($NSMData -eq $null) -or ($NSMData.Count -eq 0) ) { throw "No VM's for VPG Group '$GroupName'" }
+
+$FullNSM = Import-Csv -Path $NSMSource
 
 $VPGName = $VPGData.ZertoVPG
 If ([string]::IsNullOrEmpty( $VPGData.ZertoReplicationPriority) ) {
@@ -182,27 +149,15 @@ If ([string]::IsNullOrEmpty( $VPGData.ZertoReplicationPriority) ) {
    $Priority = $VPGData.ZertoReplicationPriority
 }
 $RecoverySiteName = $VPGData.ZertoRecoverySiteName
-$RecoverySiteID = Get-ZertoSiteID -ZertoSiteName ($RecoverySiteName)
-
 $HostClusterName  = $VPGData.ZertoHostClusterName
-$HostClusterID = Get-ZertoSiteHostClusterID -ZertoSiteIdentifier $RecoverySiteID -HostClusterName $HostClusterName
-
 $DatastoreClusterName = $VPGData.ZertoDatastoreClusterName
-$DatastoreClusterID  = Get-ZertoSiteDatastoreClusterID -ZertoSiteIdentifier $RecoverySiteID -DatastoreClusterName $DatastoreClusterName
-
 $DatastoreName =  Get-Datastore -RecoverySiteName $RecoverySiteName -ZertoDatastoreClusterName $DatastoreClusterName
-$DatastoreID  = Get-ZertoSiteDatastoreID -ZertoSiteIdentifier $RecoverySiteID -DatastoreName $DatastoreName
-
 $Network = $VPGData.ZertoFailoverNetwork
-$NetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier $RecoverySiteID -NetworkName $Network
-
 $TestNetwork = $VPGData.ZertoTestNetwork
-$TestNetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier $RecoverySiteID -NetworkName $TestNetwork
-
 $DefaultFolder = $VPGData.ZertoRecoveryFolder
-$DefaultFolderID = Get-ZertoSiteFolderID -ZertoSiteIdentifier $RecoverySiteID -FolderName $DefaultFolder
 
 #Display some VPG Information
+
 Write-Host "Creating VPG '$VPGName' with destination '$RecoverySiteName'"
 Write-Host "  Cluster:`t`t`t $HostClusterName"
 Write-Host "  DatastoreCluster:`t $DatastoreClusterName"
@@ -213,11 +168,12 @@ Write-Host "  DefaultFolder:`t $DefaultFolder"
 
 
 #Create our array of VMs'
-$AllVMS = @()
+$VMCount = 0
+$VMErrors = 0
 $NSMData | ForEach-Object {
     $VMName =  $_.Name
 
-    Write-Host "Adding VM: " $VMName
+    Write-Host "`nAdding VM: " $VMName
     Write-Host "  IPAddress`t`t" $_.($MigrationType + 'EventIPAddress')
     Write-Host "  SubnetMask`t" $_.($MigrationType + 'EventSubnetMask')
     Write-Host "  Gateway`t`t" $_.($MigrationType + 'EventGateway')
@@ -225,37 +181,24 @@ $NSMData | ForEach-Object {
     Write-Host "  DNS2`t`t`t" $_.($MigrationType + 'EventDNS2')
     Write-Host "  DNSSuffix`t`t" $_.DNSSuffix
 
+    #Check our IP 
+    $NewIPAddress = $_.($MigrationType + 'EventIPAddress')
+    $AnyDupeInNSM = $FullNSM.Where( { ($_.($MigrationType + 'EventIPAddress') -eq "$NewIPAddress") -and
+                                       ($_.Name -NE "$VMname") } ) 
+    if ( $AnyDupeInNSM.Count -gt 0 ) {
+        Write-Host "  *** ERROR - '$MigrationType' IPAddress also exists in server " $AnyDupeInNSM[0].Name
+        $VMErrors++
+    }
+
     #Throw on error - 
     try {
-        if ( [System.String]::IsNullOrEmpty( $_.($MigrationType + 'TestIPAddress') ) ) {
-            $IP = New-ZertoVPGFailoverIPAddress -NICName 'Network adapter 1' `
-                                        -IPAddress  $_.($MigrationType + 'EventIPAddress') `
-                                        -SubnetMask $_.($MigrationType + 'EventSubnetMask') `
-                                        -Gateway    $_.($MigrationType + 'EventGateway') `
-                                        -DNS1       $_.($MigrationType + 'EventDNS1') `
-                                        -DNS2       $_.($MigrationType + 'EventDNS2') `
-                                        -DNSSuffix  $_.DNSSuffix
-        } else {
+        if ( -not [System.String]::IsNullOrEmpty( $_.($MigrationType + 'TestIPAddress') ) ) {
             Write-Host "  Test IPAddress:`t`t" $_.($MigrationType + 'TestIPAddress')
             Write-Host "  Test SubnetMask:`t" $_.($MigrationType + 'TestSubnetMask')
             Write-Host "  Test Gateway:`t`t" $_.($MigrationType + 'TestGateway')
             Write-Host "  Test DNS1:`t`t`t" $_.($MigrationType + 'TestDNS1')
             Write-Host "  Test DNS2:`t`t`t" $_.($MigrationType + 'TestDNS2')
             Write-Host "  Test DNSSuffix:`t`t" $_.DNSSuffix
-
-            $IP = New-ZertoVPGFailoverIPAddress -NICName 'Network adapter 1' `
-                                        -IPAddress  $_.($MigrationType + 'EventIPAddress') `
-                                        -SubnetMask $_.($MigrationType + 'EventSubnetMask') `
-                                        -Gateway    $_.($MigrationType + 'EventGateway') `
-                                        -DNS1       $_.($MigrationType + 'EventDNS1') `
-                                        -DNS2       $_.($MigrationType + 'EventDNS2') `
-                                        -DNSSuffix  $_.DNSSuffix `
-                                        -TestIPAddress  $_.($MigrationType + 'TestIPAddress') `
-                                        -TestSubnetMask $_.($MigrationType + 'TestSubnetMask') `
-                                        -TestGateway    $_.($MigrationType + 'TestGateway') `
-                                        -TestDNS1       $_.($MigrationType + 'TestDNS1') `
-                                        -TestDNS2       $_.($MigrationType + 'TestDNS2') `
-                                        -TestDNSSuffix  $_.DNSSuffix
         }
     } catch {
         Throw ("***ERROR with $VMName : " + $Error[0])
@@ -264,59 +207,29 @@ $NSMData | ForEach-Object {
     #Override Network
     if ( -not [System.String]::IsNullOrEmpty( $_.($MigrationType + 'VPG:ZertoFailoverNetworkOverride') ) ) {
         $OverrideNetwork =  $_.($MigrationType + 'VPG:ZertoFailoverNetworkOverride')
-        Write-Host "  *** Overriding Network to: $OverrideNetwork"
-        $IP.NetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier (Get-ZertoSiteID -ZertoSiteName ($RecoverySiteName)) `
-                            -NetworkName ( $_.($MigrationType + 'VPG:ZertoFailoverNetworkOverride'))
+        Write-Host "  * Overriding Network to: $OverrideNetwork"
     }
     #if ( -not [System.String]::IsNullOrEmpty( $_.($MigrationType + 'VPG:ZertoTestNetworkOverride') ) ) {
     #    $OverrideTestNetwork =  $_.($MigrationType + 'VPG:ZertoTestNetworkOverride')
     #    Write-Host "  *** Overriding Test Network to: $OverrideTestNetwork"
-    #    $IP.TestNetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier (Get-ZertoSiteID -ZertoSiteName ($RecoverySiteName)) `
-    #                        -NetworkName ( $_.($MigrationType + 'VPG:ZertoTestNetworkOverride'))
     #}
 
     #Set our datastore based off of $VPGData.ZertoRecoverySiteName
     $VMDatastore = Get-Datastore -RecoverySiteName $RecoverySiteName -ZertoDatastoreClusterName $DatastoreClusterName
     Write-Host "  VM Datastore:`t $VMDatastore"
 
-    $Recovery = New-ZertoVPGVMRecovery -FolderIdentifier $DefaultFolderID `
-                         -DatastoreIdentifier (Get-ZertoSiteDatastoreID -ZertoSiteIdentifier $RecoverySiteID -DatastoreName $VMDatastore) 
-
     #Override folder
     if ( -not [System.String]::IsNullOrEmpty( $_.($MigrationType + 'VPG:ZertoRecoveryFolderOverride') ) ) {
         $OverrideFolder =  ( $_.($MigrationType + 'VPG:ZertoRecoveryFolderOverride') )
-        Write-Host "  *** Overriding Folder to: $OverrideFolder"
-        $Recovery.FolderIdentifier = Get-ZertoSiteFolderID  -ZertoSiteIdentifier $RecoverySiteID -FolderName $OverrideFolder
+        Write-Host "  * Overriding Folder to: $OverrideFolder"
     }
-
-    $VM = New-ZertoVPGVirtualMachine -VMName $_.Name  -VPGFailoverIPAddress $IP -VPGVMRecovery $Recovery 
-    $AllVMS += $VM
+    $VMCount++
 }
 
-Write-Host ("Adding " + $AllVMS.Count + " VMs")
-
-# Create our VPG
-If (-not $CommitVPG) {
-    Add-ZertoVPG -Priority $Priority  `
-                -VPGName $VPGName `
-                -RecoverySiteName $RecoverySiteName `
-                -ClusterName  $HostClusterName `
-                -FailoverNetwork  $Network  `
-                -TestNetwork $TestNetwork `
-                -DatastoreName $DatastoreName `
-                -JournalUseDefault $true `
-                -Folder $DefaultFolder `
-                -VPGVirtualMachines $AllVMS `
-                -DumpJSON
+if ($VMErrors -gt 0) {
+    Write-Host "`n**********************************************"
+    Write-Host "*** $VMCount VMs for this group with $VMErrors errors"
+    Write-Host "**********************************************"
 } else {
-    Add-ZertoVPG -Priority $Priority  `
-                -VPGName $VPGName `
-                -RecoverySiteName $RecoverySiteName `
-                -ClusterName  $HostClusterName `
-                -FailoverNetwork  $Network  `
-                -TestNetwork $TestNetwork `
-                -DatastoreName $DatastoreName `
-                -JournalUseDefault $true `
-                -Folder $DefaultFolder `
-                -VPGVirtualMachines $AllVMS
+    Write-Host "`n$VMCount VMs for this group"
 }
--- Test-Scripts.ps1
+++ Test-Scripts.ps1
@@ -1,1 +1,4 @@
-﻿.\Make-GroupVPG.ps1 -GroupName 'GROUP00-CHAPDA-T2' -MigrationType Mig -ZertoSourceServer 'il1zerto.nuveen.com' -ZertoUser 'nuveen\ep_lewichr'
+﻿.\Make-GroupVPG.ps1 -GroupName 'GROUP00-CHAPDA-T2' `
+                    -MigrationType Mig `
+                    -ZertoSourceServer 'il1zerto.nuveen.com' `
+                    -ZertoUser 'nuveen\ep_lewichr'
