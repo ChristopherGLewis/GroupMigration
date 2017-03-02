ede230b109129ee150816f07f25dcc73eaa61fde
Lewis <Chris.Lewis@nuveen.com>
2017-03-02 08:23:52 -0600

    update 1.2 docs and validations

--- Make-GroupVPG.ps1
+++ Make-GroupVPG.ps1
@@ -1,4 +1,66 @@
-﻿#Requires -Version 5.0
+﻿<#
+.SYNOPSIS
+Creates a Zerto VPG for the given migration group
+
+.DESCRIPTION
+The Make-GroupVPG.ps1 script creates a VPG for a given migration group.  The Script reads the VPG from the VPGList and then 
+processes each VM in the NSM to grab destination IP information.
+
+.PARAMETER GroupName 
+Name of the Zerto VPG Migration Group.  Typically in the form Group01-CHAPDA or Group01-DENPDA
+
+.PARAMETER MigrationType 
+The migration type:
+    MIG -> initial migration from FP to CHA/DEN 
+    FP -> Migration back from CHA/DEN to FP as our parachute replication
+    DR -> end state DR from CHAPDA to DENPDA
+
+.PARAMETER ZertoSourceServer  
+FQDN of the Zerto Source Server
+
+.PARAMETER ZertoUser   
+User for the Zerto source server
+
+.PARAMETER ZertoSourceServerPort 
+Zerto Source Server port - defaults to 9669
+
+.PARAMETER CommitVPG
+Switch to commit the VPG.  Without this switch, the script just processes without creating the VPG.  It does dump the 
+REST API Json for diagnostics
+
+.PARAMETER Ver
+Displays the version number and exits
+
+.EXAMPLE 
+#This DOES NOT commit the VPG
+
+.\Make-GroupVPG.ps1 -GroupName 'GROUP01-CHAPDA' `
+                    -MigrationType Mig `
+                    -ZertoSourceServer 'il1zerto.nuveen.com' `
+                    -ZertoUser 'nuveen\e_lewiCG'
+
+.EXAMPLE 
+#This commits the VPG
+
+.\Make-GroupVPG.ps1 -GroupName 'GROUP01-CHAPDA' `
+                    -MigrationType Mig `
+                    -ZertoSourceServer 'il1zerto.nuveen.com' `
+                    -ZertoUser 'nuveen\e_lewiCG' `
+                    -CommitVPG
+
+.NOTES
+Author: Chris Lewis
+Date: 2017-03-02
+Version: 1.2
+History:
+    1.0 Initial create
+    1.1 Dynamic Datastores
+    1.2 Validation of rights to NSM/VPG files, Zerto rights
+
+
+#>
+
+#Requires -Version 5.0
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 param(
     [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "VPG Group Name")] [String] $GroupName,
@@ -7,7 +69,7 @@ param(
     [parameter(Mandatory = $False, ParameterSetName = 'Default', HelpMessage = "Zerto Source Server Port")] [int] $ZertoSourceServerPort = 9669,
     [parameter(Mandatory = $true,  ParameterSetName = 'Default', HelpMessage = "Zerto Source User")] [String] $ZertoUser,
     [parameter(Mandatory = $False, ParameterSetName = 'Default', HelpMessage = "Commit VPG")] [Switch] $CommitVPG,
-    [parameter(Mandatory = $true,  ParameterSetName = 'Version', HelpMessage = "Version")] [Switch] $ver
+    [parameter(Mandatory = $true,  ParameterSetName = 'Version', HelpMessage = "Version")] [Switch] $Ver
 )
 #Add-Type -TypeDefinition "public enum VPGMigrationType { MigVPG, DRVPG, FPVPG }"
 
@@ -54,7 +116,7 @@ function Get-Datastore {
     return $DatastoreName
 }
 
-$ScriptVersion = "1.1"
+$ScriptVersion = "1.2"
 $MinZertoModuleVersion = [Version]"0.9.13"
 
 if ( $ver) {
@@ -68,18 +130,38 @@ if ( $ver) {
 #AutoImport doesn't work for Script modules
 #Import-Module ZertoModule
 Import-Module C:\Scripts\Zerto\ZertoModule\ZertoModule.psd1
+If ((get-module -Name 'ZertoModule') -eq $null ) {
+    throw "Could not find ZertoModule - please install it first via 'Install-Module -Name ZertoModule'"
+    Return
+}
 If ( (Get-Module 'ZertoModule').Version -lt $MinZertoModuleVersion ) {
     throw "You must use at least ZertoModule $MinZertoModuleVersion.  Please upgrade with 'Update-Module ZertoModule'"
     Return
 }
 
-If ((get-module -Name 'ZertoModule') -eq $null ) {
-    throw "Could not find ZertoModule - please install it first via 'Install-Module -Name ZertoModule'"
+#Log Into Zerto
+Set-Item ENV:ZertoServer $ZertoSourceServer
+Set-Item ENV:ZertoPort $ZertoSourceServerPort
+Set-ZertoAuthToken -ZertoUser $ZertoUser
+
+Try {
+    Get-ZertoLocalSite
+} catch {
+    throw "Cound not log onto ZertoServer at '$ZertoSourceServer'"
     Return
 }
 
 $NSMSource = '\\nuveen.com\Departments\EO\Logs\Servers\NSM.csv'
 $VPGSource = '\\nuveen.com\Departments\EO\Logs\Servers\ZertoVPGs.csv'
+
+If ( -not (Test-Path $NSMSource) ) {
+    throw "Cound not find NSMSource at '$NSMSource'"
+    Return
+}
+If ( -not (Test-Path $NSMSource) ) {
+    throw "Cound not find VPGSource at '$VPGSource'"
+    Return
+}
 
 $VPGData = Import-Csv -Path $VPGSource | Where-Object {$_.ZertoVPG -eq $GroupName}
 If ($VPGData -eq $null) { throw "Invalid VPG Group Name" }
@@ -92,12 +174,6 @@ if ($MigrationType -eq  'Mig') {
     $NSMData = Import-Csv -Path $NSMSource | Where-Object {$_.FPVPG -eq $GroupName}
 }
 If ( ($NSMData -eq $null) -or ($NSMData.Count -eq 0) ) { throw "No VM's for VPG Group '$GroupName'" }
-
-#Log Into Zerto
-Set-Item ENV:ZertoServer $ZertoSourceServer
-Set-Item ENV:ZertoPort $ZertoSourceServerPort
-Set-ZertoAuthToken -ZertoUser $ZertoUser
-
 
 $VPGName = $VPGData.ZertoVPG
 If ([string]::IsNullOrEmpty( $VPGData.ZertoReplicationPriority) ) {
@@ -193,6 +269,12 @@ $NSMData | ForEach-Object {
         $IP.NetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier (Get-ZertoSiteID -ZertoSiteName ($RecoverySiteName)) `
                             -NetworkName ( $_.($MigrationType + 'VPG:ZertoFailoverNetworkOverride'))
     }
+    #if ( -not [System.String]::IsNullOrEmpty( $_.($MigrationType + 'VPG:ZertoTestNetworkOverride') ) ) {
+    #    $OverrideTestNetwork =  $_.($MigrationType + 'VPG:ZertoTestNetworkOverride')
+    #    Write-Host "  *** Overriding Test Network to: $OverrideTestNetwork"
+    #    $IP.TestNetworkID = Get-ZertoSiteNetworkID -ZertoSiteIdentifier (Get-ZertoSiteID -ZertoSiteName ($RecoverySiteName)) `
+    #                        -NetworkName ( $_.($MigrationType + 'VPG:ZertoTestNetworkOverride'))
+    #}
 
     #Set our datastore based off of $VPGData.ZertoRecoverySiteName
     $VMDatastore = Get-Datastore -RecoverySiteName $RecoverySiteName -ZertoDatastoreClusterName $DatastoreClusterName
